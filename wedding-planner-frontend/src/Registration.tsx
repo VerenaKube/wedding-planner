f5ff27e4ea960d39d98bbb371a3850649a0cb717
Verena Kube <verena_stech@web.de>
2025-03-22 00:08:26 +0100

    Registrierungsseite Design erstellt

---
+++ wedding-planner-frontend/src/Registration.tsx
@@ -0,0 +1,79 @@
+import logo from "./assets/images/logo.png";
+import {useNavigate} from "react-router-dom";
+
+export default function Registration() {
+    const navigate = useNavigate();
+
+    const switchToWelcome = () => {
+        navigate('/welcome');
+    }
+
+    return (
+        <div className="h-screen w-full bg-[rgb(97,30,38)] flex flex-col justify-center "
+             style={{width: '100vw', height: '100vh'}}>
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    src={logo}
+                    alt={"Sitzplangenerator"}
+                    className="mx-auto h-40 w-auto invert brightness-100"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-white">
+                    Account erstellen
+                </h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div className="space-y-4">
+                    <div>
+                        <label className="block text-sm font-medium text-white">
+                            Vor- und Nachname
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                className="hover:border-black block w-full rounded-md bg-white px-3 py-1.5 text-base text-red-900 border-1 border-transparent outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline- focus:outline-white-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-white">
+                            Email Adresse
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                className="hover:border-black block w-full rounded-md bg-white px-3 py-1.5 text-base text-red-900 border-1 border-transparent outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline- focus:outline-white-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-white">
+                            Passwort
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                className="hover:border-black block w-full rounded-md bg-white px-3 py-1.5 text-base text-red-900 border-1 border-transparent outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline- focus:outline-white-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-white">
+                            Passwort wiederholen
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                className="hover:border-black block w-full rounded-md bg-white px-3 py-1.5 text-base text-red-900 border-1 border-transparent outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline- focus:outline-white-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mt-12">
+                        <button
+                            onClick={switchToWelcome}
+                            type="submit"
+                            className=" hover:!border-black block w-full rounded-md bg-white px-3 py-1.5 text-base text-red-900 outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-3 focus:outline-white-600 sm:text-sm">
+                            Registrieren
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
