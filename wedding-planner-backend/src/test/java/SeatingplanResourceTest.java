61863c26c258eeb99123e90f1f2ff9bd95098ac2
Rene Kube <rene.kube@live.de>
2025-03-30 13:29:17 +0200

    first quarkus-test for create endpoint

--- wedding-planner-backend/src/test/java/SeatingplanResourceTest.java
+++ wedding-planner-backend/src/test/java/SeatingplanResourceTest.java
@@ -1,4 +1,50 @@
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
 
 @QuarkusTest
-class SeatingplanResourceTest {}
+class SeatingplanResourceTest {
+
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.5");
+
+    @BeforeAll
+    static void setUp() {
+        mongoDBContainer.start();
+        System.setProperty("quarkus.mongodb.connection-string", mongoDBContainer.getReplicaSetUrl());
+    }
+
+    @AfterAll
+    static void tearDown() {
+        mongoDBContainer.stop();
+    }
+
+    @Test
+    void testCreateSeatingplan() {
+        var seatingplanCreationRequest =
+                """
+        {
+            "name": "First Example",
+            "bride": "Verena",
+            "groom": "Rene",
+            "weddingDate": "2025-05-05"
+        }
+        """;
+        var seatingplanId = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(seatingplanCreationRequest)
+                .when()
+                .post("/seatingplans")
+                .then()
+                .statusCode(201)
+                .extract()
+                .body()
+                .asString();
+
+        Assertions.assertThat(seatingplanId).isNotNull();
+    }
+}
